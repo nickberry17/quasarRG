429ff7c4146353c25dc1477085ef095c1467f58a
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2021-07-29 14:53:07 +1000

    change from keep-alive to router-view with v-slot

--- study/src/layouts/MainLayout.vue
+++ study/src/layouts/MainLayout.vue
@@ -70,9 +70,14 @@
     </q-drawer>
 
     <q-page-container>
-      <keep-alive>
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
+      <!--       <keep-alive>
         <router-view />
-      </keep-alive>
+      </keep-alive> -->
     </q-page-container>
   </q-layout>
 </template>
@@ -86,42 +91,6 @@ const linksList = [
     caption: "quasar.dev",
     icon: "school",
     link: "#/FresnelCalc",
-  },
-  {
-    title: "Github",
-    caption: "github.com/quasarframework",
-    icon: "code",
-    link: "https://github.com/quasarframework",
-  },
-  {
-    title: "Discord Chat Channel",
-    caption: "chat.quasar.dev",
-    icon: "chat",
-    link: "https://chat.quasar.dev",
-  },
-  {
-    title: "Forum",
-    caption: "forum.quasar.dev",
-    icon: "record_voice_over",
-    link: "https://forum.quasar.dev",
-  },
-  {
-    title: "Twitter",
-    caption: "@quasarframework",
-    icon: "rss_feed",
-    link: "https://twitter.quasar.dev",
-  },
-  {
-    title: "Facebook",
-    caption: "@QuasarFramework",
-    icon: "public",
-    link: "https://facebook.quasar.dev",
-  },
-  {
-    title: "Quasar Awesome",
-    caption: "Community Quasar projects",
-    icon: "favorite",
-    link: "https://awesome.quasar.dev",
   },
 ];
 
