9b49f80732529b6d914b1672cb7226263e873459
Nicholas Smith <nicholas.smith@telcoantennas.com.au>
2021-08-04 09:20:26 +1000

    change app name

--- study/quasar.conf.js
+++ study/quasar.conf.js
@@ -132,8 +132,8 @@ module.exports = configure(function (ctx) {
       },
 
       manifest: {
-        name: `Quasar App`,
-        short_name: `Quasar App`,
+        name: `Report Generator`,
+        short_name: `Report Generator`,
         description: `A Quasar Framework app`,
         display: 'standalone',
         orientation: 'portrait',
